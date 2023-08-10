dc1ac8c84fa7ff8a92a99f043a6afd3e4425795c
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-10 11:54:46 +0200

    Use proven approach

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,23 +1,21 @@
 @echo on
 
-@rem source.url in meta.yaml references a tgz file which contains a fully-built pnpm-licenses
-@rem version, we now want to turn this back into a tgz file using pnpm pack and install it
-@rem globally from that.
-cmd /c pnpm install
+md %LIBRARY_PREFIX%\share\pnpm-licenses
+pushd %LIBRARY_PREFIX%\share\pnpm-licenses
+md node_modules
+cmd /c "npm install --save pnpm-licenses@%PKG_VERSION%"
 if errorlevel 1 exit 1
+popd
 
-cmd /c pnpm pack
+pushd %LIBRARY_PREFIX%\bin
+for %%c in (pnpm) do (
+  echo @echo on >> %%c.bat
+  echo node %LIBRARY_PREFIX%\share\pnpm\node_modules\pnpm\bin\%%c "%%*" >> %%c.bat
+popd
+
+cmd /c pnpm install --prod
 if errorlevel 1 exit 1
-
-
-@rem install pnpm-licenses globally from file (as opposed to from a registry as you'd do normally)
-call npm config set prefix %BUILD_PREFIX%
-if errorlevel 1 exit 1
-
-call npm install --userconfig nonexistentrc --global %PKG_NAME%-v%PKG_VERSION%.tgz
-if errorlevel 1 exit 1
-
 
 @rem generate license disclaimer for pnpm-licenses itself :)
-call pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 if errorlevel 1 exit 1
