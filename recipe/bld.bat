32fdefa5d94ee4ab1b9e479742c0c455e108fb02
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-10 17:28:38 +0200

    Fix path to executable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,15 +7,10 @@ cmd /c "npm install --save @quantco/pnpm-licenses@%PKG_VERSION%"
 if errorlevel 1 exit 1
 popd
 
-dir %PREFIX%\Library\share\pnpm-licenses\node_modules\.bin
-dir %PREFIX%\Library\share\pnpm-licenses\node_modules\@quantco\pnpm-licenses
-dir %PREFIX%\Library\share\pnpm-licenses\node_modules\@quantco\pnpm-licenses\dist
-exit 1
-
 pushd %LIBRARY_PREFIX%\bin
 for %%c in (pnpm-licenses) do (
   echo @echo on >> %%c.bat
-  echo node %LIBRARY_PREFIX%\share\pnpm-licenses\node_modules\@quantco\pnpm-licenses\bin\%%c "%%*" >> %%c.bat
+  echo node %LIBRARY_PREFIX%\share\pnpm-licenses\node_modules\.bin\%%c "%%*" >> %%c.bat
 )
 popd
 
