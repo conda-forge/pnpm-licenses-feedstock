96733276abb2ca644f322f61de7940961978b4f2
Jannik Wibker <jannikwibker@gmail.com>
2023-08-13 13:55:57 +0200

    hopefully fix pnpm-licenses install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,24 @@
-@echo off
+@echo on
 
 md %LIBRARY_PREFIX%\share\pnpm-licenses
 pushd %LIBRARY_PREFIX%\share\pnpm-licenses
 md node_modules
-cmd /c "npm install --save @quantco/pnpm-licenses@%PKG_VERSION%"
+cmd /c "npm install @quantco/pnpm-licenses@%PKG_VERSION%"
 if errorlevel 1 exit 1
 popd
 
 pushd %LIBRARY_PREFIX%\bin
 for %%c in (pnpm-licenses) do (
-  echo @echo on >> %%c.bat
-  echo %LIBRARY_PREFIX%\share\pnpm-licenses\node_modules\.bin\%%c.cmd "%%*" >> %%c.bat
+  echo @echo off >> %%c.bat
+  echo "%LIBRARY_PREFIX%\share\pnpm-licenses\node_modules\.bin\%%c.cmd" %%* >> %%c.bat
 )
 popd
 
 cmd /c pnpm install --prod
+if errorlevel 1 exit 1
+
+@rem test if pnpm-licenses has been installed and is executable
+cmd /c pnpm-licenses help
 if errorlevel 1 exit 1
 
 @rem generate license disclaimer for pnpm-licenses itself :)
